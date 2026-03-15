53bdb9c8ff586dfaf561529afb8ce50f77dbfe64
Kris Scott <kscott91@gmail.com>
2026-03-15 10:00:56 -0500

    Add skip-main-menu level config option

--- project/scenes/levels/blank/blank_level.gd
+++ project/scenes/levels/blank/blank_level.gd
@@ -1,9 +1,6 @@
 extends Node
 
-func _init() -> void:
-	@warning_ignore("standalone_expression")
-	gdserde
-
 
 func _process(_delta: float) -> void:
+	print("Blank level is quitting")
 	get_tree().quit()
