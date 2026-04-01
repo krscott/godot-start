4bfa05478ee63e9b3ab1bc9de418d36fe64db3d7
Kris Scott <kscott91@gmail.com>
2026-04-01 06:08:21 -0700

    Refactor project file structure

---
+++ project/common/components/level_config.gd
@@ -0,0 +1,17 @@
+## For changing Overlay settings for only a single level.
+## This is so demo levels can be independent--you'll likely want to remove it
+## after initializing the template and just change Overlay defaults directly.
+class_name LevelConfig
+extends Node
+
+@export var mouse_capture := false
+@export var skip_main_menu := false
+@export var stretch_filter := false
+
+
+func _ready() -> void:
+	overlay.pause_menu_system.capture_mouse = mouse_capture
+	overlay.pause_menu_system.show_on_start = not skip_main_menu
+	overlay.stretch_filter.visible = stretch_filter
+
+	queue_free()
