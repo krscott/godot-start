bfd7adb9aeddfc3f071aa24a17be707f0c304fe2
Aidan Fleischer <aidan@Aidans-MacBook-Air.local>
2026-04-06 14:05:45 -0700

    Adding screen fade from fishing jam

--- project/autoload/overlay/overlay.gd
+++ project/autoload/overlay/overlay.gd
@@ -6,6 +6,7 @@ extends Node
 @onready var player_input: PlayerInput = %PlayerInput
 @onready var stretch_filter: CanvasLayer = %StretchFilter
 @onready var dialogue_layer: DialogueLayer = %DialogueLayer
+@onready var screen_fade: ScreenFade = %ScreenFade
 
 # Public Methods
 
@@ -23,6 +24,7 @@ func _ready() -> void:
 	assert(player_input)
 	assert(stretch_filter)
 	assert(dialogue_layer)
+	assert(screen_fade)
 
 	util.printdbg("DEBUG BUILD")
 
