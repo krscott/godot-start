e781e6aa785bc9ba060c56685ed721f9663cfdbc
Kris Scott <kscott91@gmail.com>
2026-04-11 13:12:03 -0700

    Workaround godot type bug

--- project/common/components/capture_mouse.gd
+++ project/common/components/capture_mouse.gd
@@ -13,5 +13,5 @@ func _ready() -> void:
 
 
 func _game_pause_changed(..._args: Array) -> void:
-	var cap := enabled and not gamestate.paused_pub.state and not gamestate.menu_open_pub.state
+	var cap: bool = enabled and not gamestate.paused_pub.state and not gamestate.menu_open_pub.state
 	util.set_mouse_captured(cap)
